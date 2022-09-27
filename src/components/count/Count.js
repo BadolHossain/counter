6ebc21959a18961d514ef8a576912f9bffaf57de
BadolHossain <badolhossain01909@gmail.com>
2022-09-27 23:13:16 +0600

    first commit

---
+++ src/components/count/Count.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{count <= 0 ? "Zero" : count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)} disabled={count == 0}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default Count;
